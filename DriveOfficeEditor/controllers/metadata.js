e167321010b464f7f1ff9c8c38bd08dbf6d419ac
Ubuntu <barak@OfficeEditor.q5tr2ucjnb2ubb2oyykhzzyrzd.fx.internal.cloudapp.net>
2020-08-12 09:58:29 +0300

    first commit and fix indexOf of .

--- DriveOfficeEditor/controllers/metadata.js
+++ DriveOfficeEditor/controllers/metadata.js
@@ -34,7 +34,7 @@ exports.loadMetadata = async (req, res, next) => {
       try {
         const fileId = req.query.template ? req.query.template : req.params.id;
         let metadata = await metadataService.getMetadata(fileId, req.user);
-        metadata.type = metadata.name.substring(metadata.name.indexOf(".") + 1, metadata.name.length).toLowerCase();
+        metadata.type = metadata.name.substring(metadata.name.lastIndexOf(".") + 1, metadata.name.length).toLowerCase();
         res.locals.metadata = metadata;
         if (res.locals.metadata.hasOwnProperty("permission")) {
           delete res.locals.metadata["permission"];
