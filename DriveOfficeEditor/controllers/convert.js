9ff256acacb623f3395e321f69f6c427a3724a07
Ubuntu <barak@DriveDocs.q5tr2ucjnb2ubb2oyykhzzyrzd.fx.internal.cloudapp.net>
2020-08-18 16:45:19 +0300

    fix

--- DriveOfficeEditor/controllers/convert.js
+++ DriveOfficeEditor/controllers/convert.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const request = require('request');
 const drive = require("../controllers/drive.js");
 
-exports.convertAndUpdateInDrive = async (fileId, newFormat, oldFormat, driveAccessToken) => {
-  const newFileName = `${process.env.CONVERT_FOLDER}/${fileId}.${newFormat}`;
+exports.convertAndUpdateInDrive = async (fileId, newFormat, oldFormat, driveAccessToken) => { 
+  const newFileName = `../${process.env.CONVERT_FOLDER}/${fileId}.${newFormat}`;
   await drive.updateFile(fileId, newFileName, driveAccessToken);
   fs.unlinkSync(newFileName);
 };
